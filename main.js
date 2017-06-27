9ff7d694925ca2e1bb21311bdaf31e60978077fa
Dustin Walker <thewalkman1234@gmail.com>
2017-06-27 12:16:06 -0500

    tutoring

---
+++ main.js
@@ -0,0 +1,34 @@
+var bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
+var vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
+
+// The number of loops to perform (what if the array changes?)
+var bandLoopCount = bands.length;
+var veggieLoopCount = veggie.length;
+// Keep track of which band we're on in the loop
+var currentBand = "";
+
+// Keep track of which veggie we're on in the loop
+var currentVeggie = "";
+
+// Get a reference to the appropriate DOM element for bands
+var bandElement = document.getElementById("boy-bands");
+
+// Get a reference to the appropriate DOM element for vegetables
+var veggieElement = document.getElementById("vegetables");
+
+// Start looping
+for (var i = 0; i < bandLoopCount; i += 1) {
+
+  
+  // Add the band names into the correct <div>
+  currentBand += bands[i] + " "
+  bandElement.innerHTML = currentBand
+};
+  
+for (var i = 0; i < veggieLoopCount; i += 1) {
+  
+  // Add the veggie names into the correct <div>
+  currentVeggie += vegetabls[i] + " ";
+  veggieElement.innerHTML = curentveggie
+
+};
